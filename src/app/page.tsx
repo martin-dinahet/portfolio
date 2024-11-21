3a9232c2cea1b34befd8f145b6e8cf71314ffdc4
Martin <martin.dinahet@proton.me>
2024-11-21 18:24:07 +0100

    removed about me section from page since it's not done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ const Page = (): React.JSX.Element => {
     <div className={cn("w-screen h-screen")}>
       <Header />
       <HeroSection />
-      <AboutMeSection />
+      {/* <AboutMeSection /> */}
     </div>
   );
 };
