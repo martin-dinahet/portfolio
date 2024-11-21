de663ff36511d376b22e4fbdebd3060c623d70af
Martin <martin.dinahet@proton.me>
2024-11-21 22:22:39 +0100

    fixed unused component

--- src/components/punpun/AboutMeSection.tsx
+++ src/components/punpun/AboutMeSection.tsx
@@ -6,7 +6,8 @@ import { Card } from "@/components/ui/card";
 export const AboutMeSection = () => {
   return (
     <>
-      <div className={cn("w-full p-6 mt-[5rem]")}>
+      {/* TODO remove the hidden once the component is done */}
+      <div className={cn("w-full p-6 mt-[5rem] hidden")}>
         <div className={cn("w-full flex justify-center")}>
           <h2 className={cn("text-3xl font-bold")}>About me</h2>
         </div>
