d2a67882c798322e61139c559b8b1a56e7fb0b0d
Martin <martin.dinahet@proton.me>
2025-01-07 17:38:08 +0100

    updated hero section

--- src/components/personal/Header.tsx
+++ src/components/personal/Header.tsx
@@ -6,7 +6,7 @@ import { ThemeSwitcher } from "@/components/personal/ThemeSwitcher";
 export const Header = () => {
   return (
     <>
-      <header className="flex items-center justify-between w-full gap-3 p-3 h-fit">
+      <header className="sticky top-0 flex items-center justify-between w-full gap-3 p-3 h-fit bg-background">
         <a href="#" className={cn(buttonVariants({ variant: "ghost" }), "text-lg")}>
           Martin Dinahet
         </a>
