52bb825d7d6c2b512c643f2cc203dfe3d7d8a250
Martin <martin.dinahet@proton.me>
2025-01-13 18:34:47 +0100

    updated hero section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/personal/Header";
-import { LaptopMinimal } from "lucide-react";
+import { CodeXml, Container, LaptopMinimal } from "lucide-react";
 
 export default () => {
   return (
@@ -14,10 +14,14 @@ export default () => {
             <p className="text-neutral-400">
               Full-stack developer with a passion for minimal design and clean code.
             </p>
-            <LaptopMinimal size={64} className="mx-auto text-neutral-400" />
+            <div className="flex justify-center w-full gap-4 text-neutral-400">
+              <LaptopMinimal />
+              <Container />
+              <CodeXml />
+            </div>
           </div>
         </section>
-        <section></section>
+        <section className="flex items-center justify-center"></section>
       </div>
     </>
   );
