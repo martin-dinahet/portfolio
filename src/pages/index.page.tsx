95aaac4e3c15bcca3e3fdae76b7366d7d4aa3e95
Martin <martin.dinahet@proton.me>
2025-07-19 23:25:26 +0200

    chore+feature(deps,index-page): installed & initialized daisy ui + put placeholder content on index page

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,8 +2,15 @@ import type { FC } from "react";
 
 export const IndexPage: FC = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="card card-border flex w-lg flex-col gap-4">
+        <div className="card-body">
+          <div className="skeleton h-32 w-full"></div>
+          <div className="skeleton h-4 w-28"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+        </div>
+      </div>
     </div>
   );
 };
