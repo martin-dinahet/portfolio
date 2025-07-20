cd1f45ef4f74883ca6656b2254beca9cc9d18ef1
Martin <martin.dinahet@proton.me>
2025-07-20 16:25:20 +0200

    chore(deps,files): installed and initialized daisyui + added placeholder in index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,15 @@ import type { FC } from "react";
 
 const HomePage: FC = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="card card-border w-lg">
+        <div className="card-body flex w-full flex-col gap-4">
+          <div className="skeleton h-32 w-full"></div>
+          <div className="skeleton h-4 w-28"></div>
+          <div className="skeleton h-4 w-full"></div>
+          <div className="skeleton h-4 w-full"></div>
+        </div>
+      </div>
     </div>
   );
 };
