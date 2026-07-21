a33c664cdb7dc2e7eddd9bfd8096e63482d74e63
Martin <martin.dinahet@proton.me>
2026-07-21 18:15:34 +0200

    chore: fixed config

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,9 @@ import { createRoot } from "react-dom/client";
 import { IndexPage } from "@/pages/index-page";
 import "@/globals.css";
 
-const root = document.getElementById("root");
-
-if (!root) {
-  throw new Error("Root element #root was not found.");
-}
-
-createRoot(root).render(
+// biome-ignore lint/style/noNonNullAssertion: <>
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <IndexPage />
-  </StrictMode>
+  </StrictMode>,
 );
